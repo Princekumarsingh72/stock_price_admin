d125b8b7678a8500628a3ffaac3e9c21a1ad29de
PrinceKumarsingh72 <rajkumar7256021@gmail.com>
2025-12-07 15:09:31 +0530

    your update message

--- src/services/pricesrequest.js
+++ src/services/pricesrequest.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 export const getPricesRequest = async () => {
-    const requesturl = `http://localhost:3001/api/get-bestprices-request`;
+    // const requesturl = `http://localhost:3001/api/get-bestprices-request`;
+    const requesturl = `https://stock-price-backend-wyzp.onrender.com/api/get-bestprices-request`;
 
     try {
         const response = await axios.get(requesturl, {
